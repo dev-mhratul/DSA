db1d8509323ad04980d9f3d219dbe475f05fefa2
dev-mhratul <dev.mhratul@gmail.com>
2026-04-29 23:57:34 +0600

    Today I learned about variables, operators, installation, and some basic operations in C++.

---
+++ Class/Chapter-2/code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // 1
+    // cout<<"ratul";
+
+    // 2
+    int age;
+    cout<<"Enter your age: ";
+    cin >> age;
+    cout << "Your age is: "<< age <<endl;
+    return 0;
+}
