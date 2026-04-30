ce4eb3147ff98b54567785680bd3296af9ce96cc
dev-mhratul <dev.mhratul@gmail.com>
2026-04-30 18:18:56 +0600

    I learned conditional operators and loops in C++, and explored some simple problems using them

---
+++ Class/Chapter-3/code.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Conditional statement 
+
+    // int marks;
+    // cout<<"Enter your marks: ";
+    // cin>>marks;
+
+    // if(marks>=90){
+    //     cout<< "You got A\n";
+    // }else if(marks>=80 && marks<90){
+    //     cout<< "B\n";
+    // }
+    // else{
+    //     cout<<"C";
+    // }
+
+
+    // loops<< while loop
+
+    // int count= 1;
+    // while(count<=5){
+    //     cout<<count;
+    //     count++;
+    // }
+    
+
+    // for loop
+    // for(int i=1; i<=5; i++){
+    //     cout<<i;
+    // }
+
+    // problem-1
+    // find odd number
+    for(int i=0; i<10; i++){
+        if(i%2 != 0){
+            cout<<"this is odd number:"<<i <<endl;
+        }
+    }
+
+    return 0;
+}
